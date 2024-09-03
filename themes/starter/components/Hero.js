cff73c43bd3d5e32d56e80dce62877052892d915
Ray <80705597+imrayyu@users.noreply.github.com>
2024-09-03 19:22:00 +0800

    Update Hero.js

--- themes/starter/components/Hero.js
+++ themes/starter/components/Hero.js
@@ -40,7 +40,8 @@ export const Hero = () => {
                     href={siteConfig('STARTER_HERO_BUTTON_1_URL', null, CONFIG)}
                     className="inline-flex items-center justify-center rounded-md bg-white px-7 py-[14px] text-center text-base font-medium text-dark shadow-1 transition duration-300 ease-in-out hover:bg-gray-2 hover:text-body-color"
                   >
-                     {siteConfig('STARTER_HERO_BUTTON_1_TEXT', null, CONFIG)}
+                    {siteConfig('STARTER_HERO_BUTTON_1_ICON', null, CONFIG) && <img className='mr-4' src={siteConfig('STARTER_HERO_BUTTON_1_ICON', null, CONFIG)}/>}
+                    {siteConfig('STARTER_HERO_BUTTON_1_TEXT', null, CONFIG)}
                   </a>
                 </li>
                 }
